0812138f5f4d4c53c15e62211cfd7819045e8de6
Erin Xu <148895066+ErinXU2004@users.noreply.github.com>
2025-07-15 11:44:32 +0800

    Update and rename encode.cpp to encode.v

---
+++ v0.1/encode.v
@@ -0,0 +1,9 @@
+module decoder (
+    input [31:0] instr,
+    output [2:0] type_id,     // EAT/EXER/WEIGH/SLEEP
+    output [19:0] imm
+);
+    assign type_id = instr[11:9];      
+    assign imm     = instr[31:12];    
+    
+endmodule
